34b4b7031fd190dd41fd72c273575763a2208337
Rebeca Tavares <tavaresrebeca980@gmail.com>
2026-01-11 00:45:43 +0000

    Adicionando funções

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		new MenuPet().iniciar();
 		
 		Gato g1 = new Gato(1, "Dan");
 		g1.mostrarDados();
